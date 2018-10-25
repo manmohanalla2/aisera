76896272ed1d3270e401e2ab937fbf58072d0195
MANMOHAN ALLA <malla@apple.com>
2018-10-25 05:21:34 -0700

    mapping for dict

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-TEST
+docker build -t aisera:0.1 .
+
+#### Scripts for deploying Aisera Fun Project
+
+main.py: the rest api by flask-restful. Plese run this file for the module.
+
+
+
+Compatibility
+=============
+
+Flask-RestPlus requires Python 3.6+, Docker.
+
+
+Docker Image Setup
+==================
+
+Build:
+
+	```
+    $ docker build -t aisera:0.1 .
+    ```
+
+Run:
+
+
+	```
+    $ docker run -d -v /home/ubuntu/mount:/tmp/ -p 5050:5050 aisera:0.1
+    ```
+
+
+1. GET all 
+
+	```http://{host-ip}/api/v1.0/parallel?url=<url>```
+
+	CURL Command:
+
+	```curl -X GET 'http://18.224.24.183/api/v1.0/parallel?url=http%3A%2F%2Fwww.gutenberg.org%2Ffiles%2F15%2Ftext%2F' \
+		  -H 'cache-control: no-cache'```
+
+	Return Value:
+
+	```
+	{
+	  	"data": "data",
+	  	"status": "success"
+	}
+	```
+
+	Reponse Status:
+
+	```
+	success : 200
+	Fail : 200
+	```
+
+2. GET all branch from any project
+
+	```http://{host-ip}/api/v1.0/sequential?url=<url>```
+
+	CURL Command:
+
+	```curl -X GET 'http://18.224.24.183/api/v1.0/sequential?url=http%3A%2F%2Fwww.gutenberg.org%2Ffiles%2F15%2Ftext%2Fmoby-000.txt' \
+  		-H 'cache-control: no-cache'```
+
+	Return Value:
+
+	```
+	{
+	  	"data": 
+		    {
+		      "preliminary": 1,
+		      "this": 6,
+		      ......
+		    }
+	}
+	```
+
+	Reponse Status:
+
+	```
+	success : 200
+	Fail : 200
+	```
+
+	Required Parameters:
+	- reponame: required
+	- groupname: required
+	- project_name: optional
--- app/challenge2.py
+++ app/challenge2.py
@@ -14,21 +14,38 @@ def get_file_content(file_url):
     return data
 
 
+def get_word_count(data, dict_data=None):
+    '''
+    return dict word count
+    '''
+    if type(data) == list:
+        if len(data) > 0:
+            lst_data = data
+            data = lst_data[0]
+            lst_data = lst_data[1:]
+            dict_data = get_word_count(lst_data, dict_data)
+        else:
+            return dict_data
+    #removing comma and dots from string
+    data = re.sub('[,.]','', data)
+    #spliting the data to list
+    lst_data = data.split()
+    if not dict_data:
+        dict_data = {} 
+    for i in lst_data:
+        dict_data[i.lower()] = dict_data.get(i.lower(), 0) + 1
+    return dict_data
+
+
 def sequential(file_url):
     '''
     return a dict
     '''
     #passing the url to get the data in file as string format
     data = get_file_content(file_url)
-    #removing comma and dots from string
-    data = re.sub('[,.]','', data)
-    #spliting the data to list
-    lst_data = data.split()          
-    dict_data = {} 
-    for i in lst_data:
-        dict_data[i.lower()] = dict_data.get(i.lower(), 0) + 1
+    dict_data = get_word_count(data)
     dict_data = json.dumps(dict_data)
-    filename = ''.join(['/tmp/sequential',get_unique_id(),'.json'])
+    filename = ''.join(['/tmp/sequential/',get_unique_id(),'.json'])
     with open(filename, "w") as file:
         file.write(dict_data)
     return dict_data
@@ -47,14 +64,16 @@ def parallel(uri):
     #creating 32 threads on 8 processes which is 4 threads each process
     pool = multiprocessing.Pool(32)
     data = pool.map(get_file_content, files)
-    data = " ".join(data)
-    filename = ''.join(['/tmp/parallel',get_unique_id(),'.json'])
+    data = get_word_count(data)
+    dict_data = json.dumps(data)
+    filename = ''.join(['/tmp/parallel/',get_unique_id(),'.json'])
     with open(filename, "w") as file:
-        file.write(data)
+        file.write(dict_data)
     pool.close()
     pool.join()
-    return data
+    return dict_data
 
 
 # print(sequenctial('http://www.gutenberg.org/files/15/text/moby-000.txt'))
 # print(parallel('http://www.gutenberg.org/files/15/text/'))
+
